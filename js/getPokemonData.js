bc9dd525aa1495f9be893c09dde5325101924993
James Woodward <jamesrobertwoodward@gmail.com>
2022-07-16 22:42:42 -0500

    some styling

--- js/getPokemonData.js
+++ js/getPokemonData.js
@@ -1,8 +1,22 @@
 getPokemon(1);
+get20Pokemon();
 searchPokemon();
 
+let arrayOfMoarPokemon = [];
+
 function searchPokemon() {
-    $('#search-by-name-btn').on("click", function() {
+    $(document).ready(function () {
+        $("#search-by-name").keyup(function (event) {
+            if (event.which === 13) {
+                $("#search-by-name-btn").click();
+            }
+        });
+        $("#submit").click(function () {
+            alert('clicked!');
+        })
+    });
+
+    $('#search-by-name-btn').on("click", function () {
         let pokemon = $('#search-by-name').val().toLowerCase();
         getPokemon(pokemon);
         $('#search-by-name').val('');
@@ -20,7 +34,6 @@ function getPokemon(idOrName) {
 }
 
 function getSanitizedData(dataBody) {
-    console.log(dataBody);
     return {
         pokemonName: dataBody.name,
         pokemonWeight: (dataBody.weight / 4.536).toFixed(2),
@@ -33,7 +46,7 @@ function getSanitizedData(dataBody) {
 function getPokemonTypes(pokemon) {
     let html = `<p class="ms-1">`;
     for (let i = 0; i < pokemon.pokemonTypes.length; i++) {
-        if(i < pokemon.pokemonTypes.length - 1) {
+        if (i < pokemon.pokemonTypes.length - 1) {
             html += `${pokemon.pokemonTypes[i].type.name}, `
         } else {
             html += `${pokemon.pokemonTypes[i].type.name}`
@@ -46,17 +59,58 @@ function getPokemonTypes(pokemon) {
 function renderSinglePokemon(pokemon) {
     //language=HTML
     $('#selected-pokemon').html(`
-        <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-center m-3" id="main-display">
             <img src="${pokemon.pokemonSprite}" alt="${capitalizeWord(pokemon.pokemonName)}" style="height: 30vh">
         </div>
-        <h3>Name: ${capitalizeWord(pokemon.pokemonName)}</h3>
-        <div class="d-flex">
+        <h3 class="text-center">Name: ${capitalizeWord(pokemon.pokemonName)}</h3>
+        <div class="d-flex justify-content-center">
             <p>Type: ${getPokemonTypes(pokemon)}</p>
         </div>
-        <p>Height: ${pokemon.pokemonHeight} ft</p>
-        <p>Weight: ${pokemon.pokemonWeight} lbs</p>
+        <p class="text-center">Height: ${pokemon.pokemonHeight} ft</p>
+        <p class="text-center">Weight: ${pokemon.pokemonWeight} lbs</p>
     `)
 }
+
+function get20Pokemon() {
+    //change offset to variable for pagination
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`)
+        //no semicolons until the end because we're chaining
+        .then(response => response.json())
+        //arrow function must have curly braces if you have more than one line of code in the body
+        .then(data => {
+            // console.log(data.results);
+            arrayOfMoarPokemon = [data.results];
+            renderMoarPokemonCards(arrayOfMoarPokemon);
+        })
+        .catch(err => console.error(err));
+}
+
+function renderMoarPokemonCards(arrayOfPokemon) {
+    arrayOfPokemon = arrayOfPokemon[0];
+    // console.log(arrayOfPokemon)
+    let html = ``;
+    let idOrName = '';
+    let currentPokemon;
+    for (let i = 0; i < arrayOfPokemon.length; i++) {
+        idOrName = arrayOfPokemon[i].name;
+        fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`)
+            //no semicolons until the end because we're chaining
+            .then(response => response.json())
+            //arrow function must have curly braces if you have more than one line of code in the body
+            .then(response => getSanitizedData(response))
+            .then(data => {
+                // console.log(data)
+                //language=HTML
+                html += `
+                <img src="${data.pokemonSprite}">
+                <h4>${data.pokemonName}</h4>
+                `
+            })
+            .catch(err => console.error(err));
+    }
+    $('#moar-pokemon').html(html);
+}
+
 
 function capitalizeWord(string) {
     return string[0].toUpperCase() + string.substring(1);
